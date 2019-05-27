68244da35b555506b452ad5641ea663476b5397a
avasaris <artvas@yandex.ru>
2019-05-27 16:59:43 +0200

    intermediate save

--- GasDist/DataBaseLayer.h
+++ GasDist/DataBaseLayer.h
@@ -5,10 +5,10 @@
 using namespace std;
 
 const string QUERY_ALL_CLIENTS ("SELECT DISTINCT Client FROM RawData");
-const string QUERY_ALL_CONTRACTS ("SELECT DISTINCT Contract, ContractGroup, OverLimitPriority, UnderLimitPriority, OffsetPlan FROM RawData WHERE Client LIKE '%s'");
+const string QUERY_ALL_CONTRACTS ("SELECT DISTINCT Contract, ContractGroup, OverLimitPriority, UnderLimitPriority, 0 FROM RawData WHERE Client LIKE '%s'");
+const string QUERY_OFFSET_FOR_ALL_CONTRACTS ("SELECT DISTINCT OffsetPlan FROM RawData WHERE Contract LIKE '%s' AND DataType LIKE 'plan'");
+const string QUERY_ALL_SQUARES("SELECT DISTINCT Square, SquareNumber, SquareGroup FROM RawData WHERE Contract LIKE '%s'");
 class Contract;
-
-static int ReturnDataIntoVector(void*, int, char**, char**);
 
 class Db {
 public:
@@ -17,6 +17,7 @@ public:
 
 	vector<string> GetClients();
 	vector<string> GetContracts(const string& client_name);
+	vector<string> GetSquares(const string& contract_name);
 
 private:
 	sqlite3* db;
@@ -24,6 +25,8 @@ private:
 
 	vector<string> SelectToVectorOfStrings(const string& query);
 };
+
+/// ================ DB ================
 
 Db::Db(const string& db_name) {
 	rc = sqlite3_open(db_name.c_str(), &this->db);
@@ -37,10 +40,22 @@ Db::~Db() {
 	sqlite3_close(db);
 }
 
-vector<string> Db::GetContracts(const string& client_name) {
-	string query;
-	query = boost::str(boost::format(QUERY_ALL_CONTRACTS) % client_name);
+vector<string> Db::GetSquares(const string& contract_name) {
+	string query = boost::str(boost::format(QUERY_ALL_SQUARES) % contract_name);
 	vector<string> query_result = SelectToVectorOfStrings(query);
+
+	return query_result;
+}
+
+vector<string> Db::GetContracts(const string& client_name) {
+	string query = boost::str(boost::format(QUERY_ALL_CONTRACTS) % client_name);
+	vector<string> query_result = SelectToVectorOfStrings(query);
+	for (auto q = query_result.begin(); q != query_result.end(); q += 5) {
+		string op_query = boost::str(boost::format(QUERY_OFFSET_FOR_ALL_CONTRACTS) % q[0]);
+
+		vector<string> op_query_result = SelectToVectorOfStrings(op_query);
+		q[4] = op_query_result.at(0);
+	}
 
 	return query_result;
 }
@@ -49,6 +64,18 @@ vector<string> Db::GetClients() {
 	vector<string> query_result = SelectToVectorOfStrings(QUERY_ALL_CLIENTS);
 
 	return query_result;
+}
+
+/// ================ UTILITIES ================
+
+static int ReturnDataIntoVector(void* data, int argc, char** argv, char** azColName) {
+	vector<string>* ret_data = (vector<string>*)data;
+
+	for (int i = 0; i < argc; i++) {
+		(*ret_data).push_back(argv[i] ? argv[i] : "NULL");
+	}
+
+	return 0;
 }
 
 vector<string> Db::SelectToVectorOfStrings(const string& query) {
@@ -63,14 +90,4 @@ vector<string> Db::SelectToVectorOfStrings(const string& query) {
 	}
 
 	return ret_vector;
-}
-
-static int ReturnDataIntoVector(void* data, int argc, char** argv, char** azColName) {
-	vector<string>* ret_data = (vector<string>*)data;
-
-	for (int i = 0; i < argc; i++) {
-		(*ret_data).push_back(argv[i] ? argv[i] : "NULL");
-	}
-
-	return 0;
 }
--- GasDist/d2.cpp
+++ GasDist/d2.cpp
@@ -18,13 +18,14 @@ const vector<string> tableStructure { "Client",
 double StringToDouble(const string&);
 
 class Square {
+public:
 	Square(string, string, vector<double>&, vector<double>&, int);
 	double GetFullFact();
 	double GetFullPlan();
 	double GetFactForDay(int);
 	double GetPlanForDay(int);
 private:
-	string address;
+	string name;
 	string number;
 	vector<double> plan;
 	vector<double> fact;
@@ -35,14 +36,7 @@ private:
 
 class Contract {
 public:
-	Contract(const string& name, const string& c_g, const int o_p, const int u_p, const double offset) 
-		: name(name), 
-		contracts_group(c_g), 
-		overlimit_priority(o_p), 
-		underlimit_priority(u_p), 
-		offset_plan(offset),
-		squares{} 
-	{ cout << endl << "Name=" << name << " CG=" << contracts_group << " O-p=" << overlimit_priority << " U-p=" << underlimit_priority << " Offset=" << offset_plan;  };
+	Contract(const string&, const string&, const int, const int, const double);
 private:
 	string name;
 	string contracts_group;
@@ -62,7 +56,7 @@ private:
 
 int main() {
 	SetConsoleOutputCP(CP_UTF8);
-	vector<string> clients_names;
+	vector<string> clients_names{};
 
 	{
 		Db data_base{ DB_NAME };
@@ -77,33 +71,42 @@ int main() {
 	return 0;
 }
 
+/// ================ CLIENTS ================
 
+Client::Client(const string& name)
+	: name{ name }, contracts{}
+{
+	vector<string> query_result{};
 
-	
-
-
-
-
-Client::Client(const string& name) : name(name), contracts{} {
-
-	Db data_base{DB_NAME};
-
-	vector<string> q_res = data_base.GetContracts(name);
-
-	for (auto i = q_res.begin(); i != q_res.end(); i+=5) {
-		Contract tmp_contract(i[0],i[1],stoi(i[2]), stoi(i[3]), StringToDouble(i[4]));
-		contracts.push_back(tmp_contract);
+	{
+		Db data_base{ DB_NAME };
+		query_result = data_base.GetContracts(name);
 	}
 
-
+	for (auto q = query_result.cbegin(); q != query_result.cend(); q+=5) {
+		Contract tmp_contract(q[0], q[1], stoi(q[2]), stoi(q[3]), StringToDouble(q[4]));
+		contracts.push_back(tmp_contract);
+	}
 }
 
-Square::Square(string new_address, string new_number, vector<double>& new_plan, vector<double>& new_fact, int new_days) {
-	this->address = new_address;
-	this->number = new_number;
-	this->days_in_month = new_days;
-	this->plan = new_plan;
-	this->fact = new_fact;
+/// ================ CONTRACTS ================
+
+Contract::Contract(const string& name, const string& c_g, const int o_p, const int u_p, const double offset)
+	: name{ name }, contracts_group{ c_g },
+	overlimit_priority{ o_p }, underlimit_priority{ u_p }, offset_plan{ offset },
+	squares{}
+{
+	cout << "Name=" << name << " CG=" << contracts_group << " O-p=" << overlimit_priority << " U-p=" << underlimit_priority << " Offset=" << offset_plan << endl;
+};
+
+/// ================ SQUARES ================
+
+Square::Square(string name, string number, vector<double>& new_plan, vector<double>& new_fact, int days_in_month)
+	: name{ name }, number{ number }, days_in_month{ days_in_month },
+	plan{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }, 
+	fact{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+{
+	cout << "Address=" << name << " Number=" << number << endl;
 }
 
 double Square::GetFactForDay(int day_of_interest) {
@@ -125,6 +128,8 @@ double Square::GetFullPlan() {
 	for (int i = 1; i <= days_in_month; ++i) ret_plan += GetPlanForDay(i);
 	return ret_plan;
 }
+
+/// ================ UTILITIES ================
 
 double StringToDouble(const string& str) {
 	std::locale lcl;
