902c36e06eee604b5ed4fdbf2a1fc05fc13e4471
avasaris <artvas@yandex.ru>
2019-07-24 00:40:21 +0200

    tool2 is on the go

--- Convertor/tool.cpp
+++ Convertor/tool.cpp
@@ -8,91 +8,95 @@
 
 using namespace std;
 
-class CSVfile {
-private:
-	string fileName;
-	map<string, int> fieldsStructure;
+namespace {
 
-public:
-	CSVfile(string fn) : fileName(fn) { initFieldsStructure() };
-	const vector<string> getHeader() const;
-	const vector<vector<string>> getContent() const;
+	class CSVfile {
+	private:
+		string fileName;
+		map<string, int> fieldsStructure;
 
-private:
-	void initFieldsStructure();
-};
+	public:
+		CSVfile(string fn) : fileName(fn) { initFieldsStructure(); };
+		const vector<string> getHeader() const;
+		const vector<vector<string>> getContent() const;
 
-void CSVfile::initFieldsStructure() {
+	private:
+		void initFieldsStructure();
+	};
 
-	getline(inpStream, row0);
-	vector<string> values = split_at_semicolon(row0);
-}
+	void CSVfile::initFieldsStructure() {
 
-vector<string> split_at_semicolon(const string& row)
-{
-	vector<string> res;
-	istringstream buf(row);
-	string s;
-	while (getline(buf, s, ';')) res.push_back(s);
-	return res;
-}
+	//	getline(inpStream, row0);
+	//	vector<string> values = split_at_semicolon(row0);
+	}
 
-int main() {
+	vector<string> split_at_semicolon(const string& row)
+	{
+		vector<string> res;
+		istringstream buf(row);
+		string s;
+		while (getline(buf, s, ';')) res.push_back(s);
+		return res;
+	}
 
-
-	// строка к файлу
-	// строка к базе
-	// открытие и чтение csv
-	// открытие базы
-	// создание соответствующей таблицы в базе
-	// закрытие базы
+	int main() {
 
 
+		// строка к файлу
+		// строка к базе
+		// открытие и чтение csv
+		// открытие базы
+		// создание соответствующей таблицы в базе
+		// закрытие базы
 
 
 
 
-	// вычислим разделитель целой и дробной части в рабочей локали
-/*	std::locale lcl;
-	const auto& facet = std::use_facet<std::numpunct<char>>(lcl);
-	char decimal_point = facet.decimal_point();
 
-	string row;
 
-	ifstream wInv("Input2.csv");
-	if (!wInv.bad()) {
-		int lineNum = 0;
-		while (getline(wInv, row)) {
-			vector<string> values = split_at_semicolon(row);
-			if (lineNum > 0) {
-				int ContractNum = 0;
-				istringstream issCN(values[0]);
-				issCN >> ContractNum;
+		// вычислим разделитель целой и дробной части в рабочей локали
+	/*	std::locale lcl;
+		const auto& facet = std::use_facet<std::numpunct<char>>(lcl);
+		char decimal_point = facet.decimal_point();
 
-				int GazPointNum = 0;
-				istringstream issGPN(values[1]);
-				issGPN >> GazPointNum;
+		string row;
 
-				for (int k = 3; k < values.size(); k++) {
-					double ftmp = 0.0;
-					replace(values[k].begin(), values[k].end(), ',', decimal_point);
-					istringstream istmp;
-					istmp.str(values[k]);
-					if (values[2] == "P") {
-						istmp >> plan[ContractNum][GazPointNum][k - 2];
-					}
-					else if (values[2] == "F") {
-						istmp >> fact0[ContractNum][GazPointNum][k - 2];
+		ifstream wInv("Input2.csv");
+		if (!wInv.bad()) {
+			int lineNum = 0;
+			while (getline(wInv, row)) {
+				vector<string> values = split_at_semicolon(row);
+				if (lineNum > 0) {
+					int ContractNum = 0;
+					istringstream issCN(values[0]);
+					issCN >> ContractNum;
+
+					int GazPointNum = 0;
+					istringstream issGPN(values[1]);
+					issGPN >> GazPointNum;
+
+					for (int k = 3; k < values.size(); k++) {
+						double ftmp = 0.0;
+						replace(values[k].begin(), values[k].end(), ',', decimal_point);
+						istringstream istmp;
+						istmp.str(values[k]);
+						if (values[2] == "P") {
+							istmp >> plan[ContractNum][GazPointNum][k - 2];
+						}
+						else if (values[2] == "F") {
+							istmp >> fact0[ContractNum][GazPointNum][k - 2];
+						}
 					}
 				}
+				lineNum++;
 			}
-			lineNum++;
+			wInv.close();
 		}
-		wInv.close();
-	}
-	else {
-		cout << "Can't open input data file" << endl;
-	}*/
+		else {
+			cout << "Can't open input data file" << endl;
+		}*/
 
-	return 0;
+		return 0;
+	}
+
 }
--- Convertor/tool2.cpp
+++ Convertor/tool2.cpp
@@ -1,26 +1,76 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <vector>
+
+namespace Const {
+	const char* const csv_file{"Input.csv"};
+
+};
 
 using namespace std;
+vector<string> split_at_semicolon(const string& row);
 
 class CSVConnect {
 private:
-	string filename;
+	ifstream& fileStream;
 
-	CSVConnect(string fn) filename(fn) {};
+	CSVConnect(ifstream& ifs) : fileStream(ifs) {}
 	CSVConnect& operator=(CSVConnect&);
 	CSVConnect(const CSVConnect&);
 public:
-	static CSVConnect c;
+	static CSVConnect& instance(const string& fileName) {
+		ifstream fileStream(fileName);
+		static CSVConnect csv_conn(fileStream);
+		return csv_conn;
+	}
+	ifstream& getFileStream() { return fileStream; };
+};
+
+class CSVfile {
+private:
+	CSVConnect& csv;
+	map <string, int> HeaderInfo;
+
+	void initHeaderInfo();
+
+public:
+	CSVfile(CSVConnect& csv) : csv(csv) {
+		initHeaderInfo();
+
+	}
+	
 
 };
 
 int main() {
+	CSVfile csv(CSVConnect::instance(Const::csv_file));
 
-
-
+	
 
 
 
 	return 0;
+}
+
+void CSVfile::initHeaderInfo()
+{
+	string row0;
+	getline(csv.getFileStream(), row0);
+	vector<string> tmpV = split_at_semicolon(row0);
+	int i = 0;
+	for (auto s : tmpV) HeaderInfo[s] = i++;
+
+}
+
+
+vector<string> split_at_semicolon(const string& row)
+{
+	vector<string> res;
+	istringstream buf(row);
+	string s;
+	while (getline(buf, s, ';')) res.push_back(s);
+	return res;
 }
